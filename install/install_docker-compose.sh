2e300e9167e1ba3e0ab2617cce973013fc352295
胡光华 <huguanghua@huhangmed.com>
2020-04-25 23:54:10 +0800

    css transitions 和 animated 测试html 及css 参考手册

--- install/install_docker-compose.sh
+++ install/install_docker-compose.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+
+sudo yum -y install epel-release
+
 sudo yum install -y python
 
 sudo yum install -y python2-pip.noarch
 
+sudo yum -y install python-pip
+
+sudo  pip install --upgrade pip
+
 sudo yum install python-devel
 
-sudo pip install ./docker_compose-1.25.4-py2.py3-none-any.whl  --ignore-installed requests
+#pip install -i https://mirrors.ustc.edu.cn/pypi/web/simple package            https://mirrors.tuna.tsinghua.edu.cn
+#pip install -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
+sudo pip install -i https://mirrors.ustc.edu.cn/pypi/web/simple # ./docker_compose-1.25.4-py2.py3-none-any.whl  --ignore-installed requests
+
+pip install   -i https://mirrors.ustc.edu.cn/pypi/web/simple  ./docker_compose-1.25.4-py2.py3-none-any.whl  --ignore-installed subprocess32
+
+
+#sudo curl -L https://get.daocloud.io/docker/compose/releases/download/1.25.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+————————————————
+版权声明：本文为CSDN博主「pushiqiang」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/pushiqiang/java/article/details/78682323
